c4e287642399ffad9636d504ef997377bf3f554b
R-Rijnbeek <robert270384@gmail.com>
2025-09-26 12:03:46 +0200

    KEEP DIGGING

---
+++ TEST/_google_sheet_configuration_settings_.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+#Those parameters must be configurated
+
+SHEET_ID ="<ID OF YOUR GOOGLE SHEET>" 
+
+SERVICE_ACCOUNT_FILE = "TEST\\service_account_file\\<NAME_OF_YOUR_SERVICE_ACCOUNT_FILE>.json"
